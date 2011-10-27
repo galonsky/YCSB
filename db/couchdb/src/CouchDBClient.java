f1ad936b030edf0d0c3159dfefca01892bcccacd
Alex Galonsky <alex@galonsky.com>
2011-10-27 14:10:12 -0400

    Read, insert, update, and main driver for testing.

--- db/couchdb/src/CouchDBClient.java
+++ db/couchdb/src/CouchDBClient.java
@@ -1,17 +1,28 @@
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 import java.util.Vector;
+import org.jcouchdb.db.Database;
 import com.yahoo.ycsb.DB;
+import com.yahoo.ycsb.DBException;
 
 
 public class CouchDBClient extends DB {
+    
+    private final static String HOST = "ubuntu";
+    
+    @Override
+    public void init() throws DBException {
+        
+    }
 
     @Override
     public int read (String table,
                      String key,
                      Set<String> fields,
                      HashMap<String, String> result) {
-        // TODO Auto-generated method stub
+        Database db = new Database(HOST, table);
+        
         return 0;
     }
 
@@ -27,20 +38,30 @@ public class CouchDBClient extends DB {
 
     @Override
     public int update (String table, String key, HashMap<String, String> values) {
-        // TODO Auto-generated method stub
+        Database db = new Database(HOST, table);
+        values.put("_id", key);
+        db.updateDocument(values);
         return 0;
     }
 
     @Override
     public int insert (String table, String key, HashMap<String, String> values) {
-        // TODO Auto-generated method stub
+        Database db = new Database(HOST, table);
+        values.put("_id", key);
+        db.createDocument(values);
         return 0;
     }
 
     @Override
     public int delete (String table, String key) {
-        // TODO Auto-generated method stub
         return 0;
+    }
+    
+    public static void main(String[] args) {
+        CouchDBClient couch = new CouchDBClient();
+        HashMap<String, String> doc = new HashMap<String, String>();
+        doc.put("poop", "pee");
+        couch.insert("test", "key", doc);
     }
 
 }
