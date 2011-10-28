e0e03dc0579f58c273341cf9a515ddc10d901f14
Alex Galonsky <alex@galonsky.com>
2011-10-28 14:41:13 -0400

    Implemented scan.

--- db/couchdb/src/CouchDBClient.java
+++ db/couchdb/src/CouchDBClient.java
@@ -1,10 +1,15 @@
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.Vector;
+import net.sf.json.JSONObject;
 import com.fourspaces.couchdb.Database;
 import com.fourspaces.couchdb.Document;
 import com.fourspaces.couchdb.Session;
+import com.fourspaces.couchdb.View;
+import com.fourspaces.couchdb.ViewResults;
 import com.yahoo.ycsb.DB;
 import com.yahoo.ycsb.DBException;
 
@@ -27,9 +32,7 @@ public class CouchDBClient extends DB {
         try {
             Database db = mySession.getDatabase(table);
             Document doc = db.getDocument(key);
-            for(String k : (Set<String>) doc.keySet()) {
-                result.put(k, doc.getString(k)); 
-            }
+            docToMap(doc, result);
             return 0;
         }
         catch (IOException e) {
@@ -43,7 +46,24 @@ public class CouchDBClient extends DB {
                      int recordcount,
                      Set<String> fields,
                      Vector<HashMap<String, String>> result) {
-        // TODO Auto-generated method stub
+        Database db = mySession.getDatabase(table);
+        View v = new View("_all_docs");
+        //v.setWithDocs(new Boolean(true));
+        // setWithDocs is broken in current couchdb4j. 
+        // Patch submitted here: https://github.com/mbreese/couchdb4j/pull/1
+        // Using hack below in the meantime.
+        String start = "%22" + startkey + "%22&include_docs=true";
+        
+        v.setLimit(new Integer(recordcount));
+        v.setStartKey(start);
+
+        List<Document> results = db.view(v).getResults();
+        for (Document doc : results) {
+            JSONObject json = doc.getJSONObject("doc");
+            HashMap<String, String> map = new HashMap<String, String>();
+            docToMap(json, map);
+            result.add(map);
+        }
         return 0;
     }
 
@@ -93,15 +113,28 @@ public class CouchDBClient extends DB {
         }
     }
     
+    private void docToMap(Map doc, HashMap<String, String> map) {
+        for(String k : (Set<String>) doc.keySet()) {
+            map.put(k, (String) doc.get(k)); 
+        }
+    }
+    
     public static void main(String[] args) throws DBException {
         CouchDBClient couch = new CouchDBClient();
         couch.init();
-        HashMap<String, String> map = new HashMap<String, String>();
-        map.put("name", "joe");
-        map.put("age", "21");
+        Vector<HashMap<String,String>> results = new Vector<HashMap<String,String>>();
+        couch.scan("test", "key", 5, null, results);
+        System.out.println(results.get(0).get("poop"));
+        
+        //HashMap<String, String> map = new HashMap<String, String>();
+        //couch.read("test", "key", null, map);
+        //System.out.println(map.get("poop"));
+        //map.put("name", "joe");
+        //map.put("age", "21");
         //couch.insert("test", "galonsky", map);
         //couch.update("test", "galonsky", map);
-        couch.delete("test", "galonsky");
+        //couch.delete("test", "galonsky");
+        //couch.scan("test", "key", 5, null, null);
         
         
     }
