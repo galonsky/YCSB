346e0b00ff8e81954ea4fdc5915f4dedfa3bf0b6
Alex Galonsky <alex@galonsky.com>
2011-12-01 17:53:35 -0500

    Stub for CassandraHBaseClient

---
+++ db/cassandra-hbase/src/com/yahoo/ycsb/db/CassandraHBaseClient.java
@@ -0,0 +1,51 @@
+package com.yahoo.ycsb.db;
+
+import java.util.HashMap;
+import java.util.Set;
+import java.util.Vector;
+import com.yahoo.ycsb.DB;
+
+public class CassandraHBaseClient extends DB {
+
+    @Override
+    public int read (String table,
+                     String key,
+                     Set<String> fields,
+                     HashMap<String, String> result) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+
+    @Override
+    public int scan (String table,
+                     String startkey,
+                     int recordcount,
+                     Set<String> fields,
+                     Vector<HashMap<String, String>> result) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+
+    @Override
+    public int update (String table, String key, HashMap<String, String> values) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+
+    @Override
+    public int insert (String table, String key, HashMap<String, String> values) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+
+    @Override
+    public int delete (String table, String key) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+}
